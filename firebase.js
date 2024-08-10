4c55bce6a21cb3b7ae68d40e859ce9af5f07645d
Josh Earl Conrad Paclibar <josh.developer.portal@gmail.com>
2024-08-10 23:31:42 +0800

    2nd commit

--- firebase.js
+++ firebase.js
@@ -1,13 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-import { GoogleAuthProvider } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBIWvYRj5DfsfeCxeozEUSSUjGjB80KhUM",
   authDomain: "inventory-management-8872a.firebaseapp.com",
@@ -18,10 +11,11 @@ const firebaseConfig = {
   measurementId: "G-GW0ZYHPR8T"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const firestore = getFirestore(app);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
+let firestore;
 
-export { auth, provider, firestore };
+if (typeof window !== 'undefined') {
+  const app = initializeApp(firebaseConfig);
+  firestore = getFirestore(app);
+}
+
+export { firestore };
